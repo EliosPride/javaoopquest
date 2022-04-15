bc2473b935ab45f77da5cc4f4b45a7f376155a6d
Illia Sydorchuk <batonvagon@gmail.com>
2022-04-15 16:04:23 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Machine truck = new Truck();
+        Machine bulldozer = new Bulldozer();
+        Machine excavator = new Excavator();
+        Machine[] autoPark = {truck, bulldozer, excavator};
+        for (Machine workStart : autoPark) {
+            workStart.doWork();
+        }
+        for (Machine workEnd : autoPark) {
+            workEnd.stopWork();
+        }
+    }
 }
